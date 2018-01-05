0e4620e0ab9b9705f8e4bad905d2ac67c6c5f3d2
yellowred <olegku@gmail.com>
2018-01-05 17:20:20 +0800

    lora server ignore generated examples

---
+++ sketch_lora_server_humidity/sketch_lora_server_humidity.ino
@@ -0,0 +1,174 @@
+/*
+  LoRa Simple Yun Server :
+  Support Devices: LG01. 
+  
+  Example sketch showing how to create a simple messageing server, 
+  with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+  reliability, so you should only use RH_RF95 if you do not need the higher
+  level messaging abilities.
+
+  It is designed to work with the other example LoRa Simple Client
+
+  User need to use the modified RadioHead library from:
+  https://github.com/dragino/RadioHead
+
+  modified 16 11 2016
+  by Edwin Chen <support@dragino.com>
+  Dragino Technology Co., Limited
+*/
+//If you use Dragino IoT Mesh Firmware, uncomment below lines.
+//For product: LG01. 
+#define BAUDRATE 115200
+
+//If you use Dragino Yun Mesh Firmware , uncomment below lines. 
+//#define BAUDRATE 250000
+
+#include <Console.h>
+#include <SPI.h>
+#include <RH_RF95.h>
+
+// Singleton instance of the radio driver
+RH_RF95 rf95;
+
+int led = A2;
+float frequency = 868.0;
+String dataString = "";
+String myWriteAPIString = "KCR6EA6G43KT1K64";
+uint16_t crcdata = 0;
+uint16_t recCRCData = 0;
+
+void setup() 
+{
+  pinMode(led, OUTPUT);     
+  Bridge.begin(BAUDRATE);
+  Console.begin();
+  while (!Console) ; // Wait for console port to be available
+  Console.println("Start Sketch");
+  if (!rf95.init())
+    Console.println("init failed");
+  // Setup ISM frequency
+  rf95.setFrequency(frequency);
+  // Setup Power,dBm
+  rf95.setTxPower(13);
+  
+  // Setup Spreading Factor (6 ~ 12)
+  rf95.setSpreadingFactor(7);
+  
+  // Setup BandWidth, option: 7800,10400,15600,20800,31200,41700,62500,125000,250000,500000
+  rf95.setSignalBandwidth(125000);
+  
+  // Setup Coding Rate:5(4/5),6(4/6),7(4/7),8(4/8) 
+  rf95.setCodingRate4(5);
+  
+  Console.print("Listening on frequency: ");
+  Console.println(frequency);
+}
+
+uint16_t calcByte(uint16_t crc, uint8_t b)
+{
+    uint32_t i;
+    crc = crc ^ (uint32_t)b << 8;
+  
+    for ( i = 0; i < 8; i++)
+    {
+      if ((crc & 0x8000) == 0x8000)
+        crc = crc << 1 ^ 0x1021;
+      else
+        crc = crc << 1;
+    }
+    return crc & 0xffff;
+}
+
+uint16_t CRC16(uint8_t *pBuffer, uint32_t length)
+{
+    uint16_t wCRC16 = 0;
+    uint32_t i;
+    if (( pBuffer == 0 ) || ( length == 0 ))
+    {
+        return 0;
+    }
+    for ( i = 0; i < length; i++)
+    {
+        wCRC16 = calcByte(wCRC16, pBuffer[i]);
+    }
+    return wCRC16;
+}
+
+uint16_t recdata( unsigned char* recbuf, int Length)
+{
+    crcdata = CRC16(recbuf, Length - 2); //Get CRC code
+    recCRCData = recbuf[Length - 1]; //Calculate CRC Data
+    recCRCData = recCRCData << 8; //
+    recCRCData |= recbuf[Length - 2];
+}
+
+void loop()
+{
+  if (rf95.available())
+  {
+    // Should be a message for us now   
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    if (rf95.recv(buf, &len))
+    {
+      recdata( buf, len);
+      digitalWrite(led, HIGH);
+//      RH_RF95::printBuffer("request: ", buf, len);
+      Console.print("got request2: ");
+      Console.println((char *)buf);
+      Console.println(len);
+      Console.print("RSSI: ");
+      Console.println(rf95.lastRssi(), DEC);
+
+      dataString ="field1=";
+      dataString += "31.00";
+      
+      uploadData();
+      dataString="";
+      
+      // Send a reply
+      uint8_t data[] = "And hello back to you";
+      rf95.send(data, sizeof(data));
+      rf95.waitPacketSent();
+      Console.println("Sent a reply");
+      digitalWrite(led, LOW);
+    }
+    else
+    {
+      Console.println("recv failed");
+    }
+  }
+}
+
+void uploadData() {//Upload Data to ThingSpeak
+  // form the string for the API header parameter:
+
+
+  // form the string for the URL parameter, be careful about the required "
+  String upload_url = "https://api.thingspeak.com/update?api_key=";
+  upload_url += myWriteAPIString;
+  upload_url += "&";
+  upload_url += dataString;
+
+  Console.println("Call Linux Command to Send Data");
+  Process p;    // Create a process and call it "p", this process will execute a Linux curl command
+  p.begin("curl");
+  p.addParameter("-k");
+  p.addParameter(upload_url);
+  p.run();    // Run the process and wait for its termination
+
+  Console.print("Feedback from Linux: ");
+  // If there's output from Linux,
+  // send it out the Console:
+  while (p.available()>0) 
+  {
+    char c = p.read();
+    Console.write(c);
+  }
+  Console.println("");
+  Console.println("Call Finished");
+  Console.println("####################################");
+  Console.println("");
+}
+
+

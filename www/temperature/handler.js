dd38ce9717120a06a3ef3560ca061fa26388bb22
yellowred <olegku@gmail.com>
2018-01-08 18:03:11 +0800

    add header

--- www/temperature/handler.js
+++ www/temperature/handler.js
@@ -6,7 +6,7 @@ module.exports.helloWorld = (event, context, callback) => {
     headers: {
       'Content-type': 'text/html'
     },
-    body: '<html><body><iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/395712/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line"></iframe> </body></html>'
+    body: '<html><body><h2>Viseo HK</h2><iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/395712/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line"></iframe> </body></html>'
   };
 
   callback(null, response);
